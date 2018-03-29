61181ef1f5e2a82523bbd8c900cf96a3ac2e20df
Ajitesh Tiwari <ajitesh.tiwari@mrcooper.com>
2018-03-29 16:25:37 +0530

    Added function interface with usage

---
+++ src/com/functional/java/BreakingBad.java
@@ -0,0 +1,8 @@
+package com.functional.java;
+
+@FunctionalInterface
+public interface BreakingBad {
+
+    void sayMyName(String name);
+
+}
--- src/com/functional/java/Main.java
+++ src/com/functional/java/Main.java
@@ -3,6 +3,11 @@ package com.functional.java;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        watchBreakingBad("Walter White", System.out::println);
+        watchBreakingBad("Jesse Pinkman", System.out::println);
+    }
+
+    private static void watchBreakingBad(String name, BreakingBad breakingBad) {
+        breakingBad.sayMyName("My name is ".concat(name));
     }
 }
